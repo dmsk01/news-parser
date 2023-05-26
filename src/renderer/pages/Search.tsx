6bbb8e366d7c14d3ab6cbc29f0fa5d14e7b09932
Dmitry Petrov <53710984+dmsk01@users.noreply.github.com>
2023-05-26 11:13:10 +0300

    move async fetchng to thunk from ui

--- src/renderer/pages/Search.tsx
+++ src/renderer/pages/Search.tsx
@@ -2,51 +2,9 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Button } from 'antd';
-import {
-  clearNews,
-  searchNews,
-  fetchNews,
-  INewsState,
-} from 'renderer/store/newsSlice';
-import { INews, INewsItem } from 'renderer/types/news';
+import { clearNews, fetchNews, INewsState } from 'renderer/store/newsSlice';
 import { ISettingsState } from 'renderer/store/settingsSlice';
 import NewsList from '../components/NewsList/NewsList';
-
-const getRssFromSrc = (rssSource: string) => {
-  return window.electron.ipcRenderer
-    .invoke('get-news', rssSource)
-    .catch(console.log);
-};
-
-function getRssDetails(src: string) {
-  return window.electron.ipcRenderer
-    .invoke('get-details', src)
-    .catch(console.log);
-}
-const isNewsResponse = (sourceResponse: unknown): sourceResponse is INews => {
-  if (!sourceResponse) return false;
-  return (
-    typeof sourceResponse === 'object' &&
-    'items' in sourceResponse &&
-    'title' in sourceResponse
-  );
-};
-
-function addSourceTitleToNewsItem({ items, title }: INews) {
-  return items.map((newsItem) => {
-    return { ...newsItem, sourceName: title };
-  });
-}
-
-const extractDetailsFromHTML = (html: string) => {
-  let text = '';
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
-  doc.querySelectorAll('p').forEach((item) => {
-    text += item.innerText;
-  });
-  return text;
-};
 
 const Search = () => {
   const dispatch = useDispatch();
@@ -62,40 +20,6 @@ const Search = () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     dispatch(fetchNews(sources));
-
-    // const requests: Promise<unknown>[] = [];
-    // sources.forEach((rssSource: string) => {
-    //   requests.push(getRssFromSrc(rssSource));
-    // });
-    // const responses = await Promise.all(requests)
-    //   .then((response) => response.filter((resp) => isNewsResponse(resp)))
-    //   .catch(console.log);
-
-    // if (Array.isArray(responses)) {
-    //   const news = responses
-    //     .reduce((acc: INewsItem[], next) => {
-    //       acc.push(...addSourceTitleToNewsItem(next as INews));
-    //       return acc;
-    //     }, [])
-    //     .slice(0, 5);
-
-    //   const fetchNewsDetais = async () => {
-    //     const updatedData = await Promise.all(
-    //       news.map(async (item) => {
-    //         if (!item.link) throw new Error('Link does not exist in news item');
-    //         const response = await getRssDetails(item.link);
-    //         const details = extractDetailsFromHTML(response as string);
-    //         return { ...item, details };
-    //       })
-    //     );
-
-    //     return updatedData;
-    //   };
-
-    //   const newsWithDetails = await fetchNewsDetais();
-
-    //   dispatch(searchNews({ news: newsWithDetails }));
-    // }
   };
 
   return (
@@ -112,7 +36,7 @@ const Search = () => {
       </Button>
       {status === 'loading' && <h2>Loading...</h2>}
       {error && <h2>An error occured {error}</h2>}
-      {/* <NewsList /> */}
+      <NewsList />
     </>
   );
 };
--- src/renderer/store/newsSlice.ts
+++ src/renderer/store/newsSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { INewsItem } from 'renderer/types/news';
+import { INews, INewsItem } from 'renderer/types/news';
 
 export interface INewsState {
   news: INewsItem[];
@@ -7,21 +7,84 @@ export interface INewsState {
   error: string | null;
 }
 
+const getRssFromSrc = (rssSource: string) => {
+  return window.electron.ipcRenderer
+    .invoke('get-news', rssSource)
+    .catch(console.log);
+};
+
+function getRssDetails(src: string) {
+  return window.electron.ipcRenderer
+    .invoke('get-details', src)
+    .catch(console.log);
+}
+
+const isNewsResponse = (sourceResponse: unknown): sourceResponse is INews => {
+  if (!sourceResponse) return false;
+  return (
+    typeof sourceResponse === 'object' &&
+    'items' in sourceResponse &&
+    'title' in sourceResponse
+  );
+};
+
+function addSourceTitleToNewsItem({ items, title }: INews) {
+  return items.map((newsItem) => {
+    return { ...newsItem, sourceName: title };
+  });
+}
+
+const extractDetailsFromHTML = (html: string) => {
+  let text = '';
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  doc.querySelectorAll('p').forEach((item) => {
+    text += item.innerText;
+  });
+  return text;
+};
+
 const initialState = { news: [], status: null, error: null } as INewsState;
 
 export const fetchNews = createAsyncThunk(
   'news/fetchNews',
-  async function (sources, { rejectWithValue }) {
-    console.log(sources);
+  async function (sources: string[], { rejectWithValue }) {
     try {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/posts?_limit=10'
-      );
-      if (!response.ok) {
-        throw new Error('Something went wrong, can not to fetch data.');
+      const requests: Promise<unknown>[] = [];
+
+      sources.forEach((rssSource: string) => {
+        requests.push(getRssFromSrc(rssSource));
+      });
+
+      const responses = await Promise.all(requests)
+        .then((response) => response.filter((resp) => isNewsResponse(resp)))
+        .catch(console.log);
+
+      if (Array.isArray(responses)) {
+        const news = responses
+          .reduce((acc: INewsItem[], next) => {
+            acc.push(...addSourceTitleToNewsItem(next as INews));
+            return acc;
+          }, [])
+          .slice(0, 5);
+
+        const fetchNewsDetais = async () => {
+          const updatedData = await Promise.all(
+            news.map(async (item) => {
+              if (!item.link)
+                throw new Error('Link does not exist in news item');
+              const response = await getRssDetails(item.link);
+              const details = extractDetailsFromHTML(response as string);
+              return { ...item, details };
+            })
+          );
+
+          return updatedData;
+        };
+
+        const newsWithDetails = await fetchNewsDetais();
+        return newsWithDetails;
       }
-      const data = await response.json();
-      return data;
     } catch (error) {
       return rejectWithValue(error.message);
     }
